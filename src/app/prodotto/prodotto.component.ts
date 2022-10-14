0bdc1c94dc9893192f38fcc7a53c42f8aa38e96f
Robert Hanu <r.hanu@itsrizzoli.it>
2022-10-14 13:16:14 +0200

    completamento eCommerce

--- src/app/prodotto/prodotto.component.ts
+++ src/app/prodotto/prodotto.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 import { prodotto } from '../data/prodotto';
+import { CarrelloService } from '../service/carrello.service';
 import { ProdottiService } from '../service/prodotti.service';
 
 @Component({
@@ -12,15 +14,32 @@ export class ProdottoComponent implements OnInit {
 
   prodotto?:prodotto
 
-  constructor(private route: ActivatedRoute, private prodottoService:ProdottiService) {
+  constructor(private route: ActivatedRoute, private authService: AuthService, private prodottiService: ProdottiService, private carrello: CarrelloService) {
     console.log(route);
     const { slug } = route?.snapshot?.params ?? {}
     //const slug = route.snapshot.params["slug"]
-    this.prodotto = prodottoService.cercaProdotto(slug)
+    this.prodotto = prodottiService.cercaProdotto(slug)
 
   }
 
   ngOnInit(): void {
+  }
+
+  onClick(prodottoCarrello: prodotto) {
+
+    if (this.authService.isAuthenticated()) {
+      this.carrello.addProdotto(prodottoCarrello);
+
+      this.carrello.calcolaTotale()
+
+      console.log(this.carrello.getCarrello());
+      alert("Prodotto aggiunto al carrello");
+    }
+    else {
+      alert("Devi essere autenticato per aggiungere un prodotto al carrello")
+    }
+
+
   }
 
 }
